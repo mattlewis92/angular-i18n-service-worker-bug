58647d5cd9f9de3c0f54ea5292678aea98c4e310
Matt Lewis <hello@mattlewis.me>
2021-03-18 12:37:31 +0000

    Put sw in root directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import {APP_BASE_HREF} from '@angular/common';
   imports: [
     BrowserModule,
     // AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('../ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [{
     provide: APP_BASE_HREF,
